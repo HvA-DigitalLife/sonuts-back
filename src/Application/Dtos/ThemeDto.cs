d03202e56eda5a9b17c4e968383e597e1e2e5338
Thomas Lem <t.lem@chippr.dev>
2022-07-19 12:18:11 +0200

    Made frequency goal optional

--- src/Application/Dtos/ThemeDto.cs
+++ src/Application/Dtos/ThemeDto.cs
@@ -13,15 +13,15 @@ public class ThemeDto : IMapFrom<Theme>
 	public string Description { get; set; } = default!;
 	public CategoryDto Category { get; set; } = default!;
 	public ImageDto Image { get; set; } = default!;
-	public FrequencyType FrequencyType { get; set; } = default!;
-	public int FrequencyGoal { get; set; } = default!;
+	public FrequencyType FrequencyType { get; set; }
+	public int? FrequencyGoal { get; set; }
 	public string CurrentQuestion { get; set; } = default!;
 	public string GoalQuestion { get; set; } = default!;
-	public bool IsRecommend { get; set; }
+	public bool IsRecommended { get; set; }
 	public ICollection<ActivityDto> Activities { get; set; } = new List<ActivityDto>();
 
 	public void Mapping(Profile profile) =>
 		profile.CreateMap<Theme, ThemeDto>()
-			.ForMember(themeDto => themeDto.IsRecommend, expression => expression
+			.ForMember(themeDto => themeDto.IsRecommended, expression => expression
 				.MapFrom(theme => theme.Name.ToUpper().Contains('B')));
 }
