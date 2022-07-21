bf9d652c68558be33cec5a9a4430956c5ce7a013
Sergio Bondietti <sergio@bondietti.nl>
2022-07-21 15:09:27 +0200

    - Cleanup of old models - Added test Dao to categories query

--- src/Infrastructure/Fhir/Models/Caregiver.cs
+++
@@ -1,9 +0,0 @@
-namespace Sonuts.Infrastructure.Fhir.Models;
-
-public class Caregiver
-{
-	public string? Id { get; set; }
-	public string? Name { get; set; }
-	public bool IsComplete { get; set; }
-	public string? Secret { get; set; }
-}
--- src/Infrastructure/Fhir/Models/Domain.cs
+++
@@ -1,9 +0,0 @@
-namespace Sonuts.Infrastructure.Fhir.Models;
-
-public class Domain
-{
-	public string? Id { get; set; }
-
-	public string? Name { get; set; }
-
-}
