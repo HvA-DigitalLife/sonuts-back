f85d3739a0bb6d498fd6c158f503462c8932b87f
Thomas Lem <t.lem@chippr.dev>
2022-08-22 16:05:22 +0200

    Added migration

---
+++ src/Infrastructure/Migrations/20220822140458_FirstLastName.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sonuts.Infrastructure.Migrations
+{
+    public partial class FirstLastName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Participants",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Participants",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Participants");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Participants");
+        }
+    }
+}
