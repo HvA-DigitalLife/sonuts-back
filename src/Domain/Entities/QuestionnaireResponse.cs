540317615b794a81ffb6aed0414f8fdde7fabb5a
Thomas Lem <t.lem@chippr.dev>
2022-10-07 16:18:13 +0200

    Fixed CreatedAt

--- src/Domain/Entities/QuestionnaireResponse.cs
+++ src/Domain/Entities/QuestionnaireResponse.cs
@@ -2,7 +2,7 @@ namespace Sonuts.Domain.Entities;
 
 public class QuestionnaireResponse : BaseEntity
 {
-	public DateTime CreatedAt { get; } = DateTime.Now;
+	public DateTime CreatedAt { get; init; } = DateTime.Now;
 	public Questionnaire Questionnaire { get; set; } = default!;
 	public Participant Participant { get; set; } = default!;
 	public ICollection<QuestionResponse> Responses { get; set; } = new List<QuestionResponse>();
