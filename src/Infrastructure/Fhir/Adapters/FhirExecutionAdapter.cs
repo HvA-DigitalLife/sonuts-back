eb7d77f5719d58a0c3548df8c1ab1b97cc82048a
Sergio Bondietti <sergio@bondietti.nl>
2023-02-21 12:11:51 +0100

    Updated FhirExecutionAdapter

--- src/Infrastructure/Fhir/Adapters/FhirExecutionAdapter.cs
+++ src/Infrastructure/Fhir/Adapters/FhirExecutionAdapter.cs
@@ -44,23 +44,62 @@ public static class FhirExecutionAdapter
 		// create plan definion and meta data
 		var fhirObservation= new Hl7.Fhir.Model.Observation{
 			Id = execution.Id.ToString(),
-			Issued =new DateTimeOffset(execution.CreatedAt.Year, execution.CreatedAt.Month, execution.CreatedAt.Day, 0, 0, 0, TimeSpan.FromHours(10)),
-			Status = execution.IsDone?Hl7.Fhir.Model.ObservationStatus.Final:Hl7.Fhir.Model.ObservationStatus.Preliminary
+			// created at
+			Issued = new DateTimeOffset(execution.CreatedAt.Year, execution.CreatedAt.Month, execution.CreatedAt.Day, 0, 0, 0, TimeSpan.FromHours(10)),
+			// is done
+			Status = execution.IsDone?Hl7.Fhir.Model.ObservationStatus.Final:Hl7.Fhir.Model.ObservationStatus.Preliminary,
+			// reason annotation
+			Note = new List<Hl7.Fhir.Model.Annotation> {
+				new Hl7.Fhir.Model.Annotation {
+					Text = new Hl7.Fhir.Model.Markdown(execution.Reason)
+				}
+			},
+			// amount component
+			Component = new List<Hl7.Fhir.Model.Observation.ComponentComponent> {	
+				new Hl7.Fhir.Model.Observation.ComponentComponent{
+					Code = new Hl7.Fhir.Model.CodeableConcept {
+						Coding = new List<Hl7.Fhir.Model.Coding> {
+							new Hl7.Fhir.Model.Coding {
+								System = "http://loinc.org",
+								Code = "90897-0",
+								Display = "Percent of task completed"
+							}
+						}
+					},
+					Value = new Hl7.Fhir.Model.Integer(execution.Amount)
+				}
+			}	
 		};
-		
 
-		
 		var serializer = new FhirJsonSerializer();
 		return serializer.SerializeToString(fhirObservation);
 	}
 
 	private static Execution FhirObservationToExecution(Hl7.Fhir.Model.Observation fhirObservation) {
 
+		// Get amount
+		int amount = 0;
+		foreach (var fhirObservationComponent in fhirObservation.Component) {
+			foreach (var fhirCoding in fhirObservationComponent.Code.Coding) {
+				if (fhirCoding.Code == "90897-0") {
+					amount =  Int32.Parse(fhirObservationComponent.Value.ToString() ?? "0");
+				}
+			}
+		}
+
+		// get reason
+		var reason = "";
+		foreach (var annotation in fhirObservation.Note) {
+			reason = annotation.Text.ToString();
+		}
+
+		// build execution object
 		var execution = new Execution {
 			Id = Guid.Parse(fhirObservation.Id),
 			CreatedAt = new DateOnly(fhirObservation.Issued.Value.Year, fhirObservation.Issued.Value.Day, fhirObservation.Issued.Value.Month),
 			IsDone = fhirObservation.Status == Hl7.Fhir.Model.ObservationStatus.Final,
-			Amount = 0, // to-do: implement
+			Amount = amount,
+			Reason = reason,
 			Goal = new Goal{ // to-do: add identifier from reference
 				Activity = new Activity{
 					Name = "",
