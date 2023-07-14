b277b692c784c566e903e1274ad44e6deb48b8f6
Stephanie Kramer <s.kramer2@hva.nl>
2023-07-14 15:27:38 +0200

    Add unit test for bone exercises

--- tests/Application.UnitTests/Logic/RecommendationTests.cs
+++ tests/Application.UnitTests/Logic/RecommendationTests.cs
@@ -57,7 +57,7 @@ namespace Sonuts.Application.UnitTests.Logic
 			QuestionResponse[] questionResponses = new QuestionResponse[1];
 			questionResponses[0] = questionResponse;
 
-			var expected = true;
+			var expected = false;
 			//Act
 
 			GetCategoriesQueryHandler getCategoriesQueryHandler = new GetCategoriesQueryHandler();
@@ -65,14 +65,6 @@ namespace Sonuts.Application.UnitTests.Logic
 
 			//Assert
 			Assert.AreEqual(expected, actual);
-		}
-
-		public ApplicationDbContext GetMemoryContext()
-		{
-			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-			.UseInMemoryDatabase(databaseName: "InMemoryDatabase")
-			.Options;
-			return new ApplicationDbContext(options);
 		}
 
 		private QuestionResponse CreateQuestionResponseForBalancingExercises()
@@ -128,6 +120,114 @@ namespace Sonuts.Application.UnitTests.Logic
 			response.QuestionnaireResponse = questionnaireResponse;
 
 			return response;
+		}
+
+		[Test]
+		public void IsRecommendedThemeCalcuationTest_Bone_Exercises()
+		{
+			//Arrange
+			var question = new Question
+			{
+				Type = QuestionType.Integer,
+				Text = "Number of days per week bone-strengthening exercises",
+				Description = null,
+				Information = null,
+				Order = 35,
+				EnableWhen = null,
+				AnswerOptions = null,
+				OpenAnswerLabel = null,
+				IsRequired = true,
+				Min = 0,
+				Max = 7
+			};
+
+			RecommendationRule balanceExercises = new RecommendationRule
+			{
+				Type = RecommendationRuleType.All,
+				Operator = Operator.GreaterOrEquals,
+				Value = "2",
+			};
+
+			balanceExercises.Questions.Add(question);
+
+			RecommendationRule[] ruleArray = new RecommendationRule[1];
+			ruleArray[0] = balanceExercises;
+
+			var questionResponse = CreateQuestionResponseForBoneExercises();
+			QuestionResponse[] questionResponses = new QuestionResponse[1];
+			questionResponses[0] = questionResponse;
+
+			var expected = true;
+			//Act
+
+			GetCategoriesQueryHandler getCategoriesQueryHandler = new GetCategoriesQueryHandler();
+			var actual = getCategoriesQueryHandler.IsRecommendedThemeCalculation(questionResponses, ruleArray);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		private QuestionResponse CreateQuestionResponseForBoneExercises()
+		{
+
+			var participant = new Participant
+			{
+				FirstName = "Jane",
+				LastName = "Doe",
+				Birth = new DateOnly(year: 1958, month: 10, day: 20),
+				Gender = "Female",
+				Weight = 76,
+				Height = 178,
+				MaritalStatus = "Married",
+				IsActive = true
+			};
+
+			var question = new Question
+			{
+				Type = QuestionType.Integer,
+				Text = "Number of days per week bone-strengthening exercises",
+				Description = null,
+				Information = null,
+				Order = 35,
+				EnableWhen = null,
+				AnswerOptions = null,
+				OpenAnswerLabel = null,
+				IsRequired = true,
+				Min = 0,
+				Max = 7
+			};
+
+			var questionnaire = new Questionnaire
+			{
+				Title = "Questions about exercise",
+				Description = "Questions about exercise"
+			};
+			questionnaire.Questions.Add(question);
+
+			var response = new QuestionResponse
+			{
+				Question = question,
+				Answer = "1"
+			};
+
+			var questionnaireResponse = new QuestionnaireResponse
+			{
+				CreatedAt = new DateOnly(year: 2023, month: 07, day: 14),
+				Questionnaire = questionnaire,
+				Participant = participant,
+			};
+			questionnaireResponse.Responses.Add(response);
+			response.QuestionnaireResponse = questionnaireResponse;
+
+			return response;
+		}
+
+		public ApplicationDbContext GetMemoryContext()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+			.UseInMemoryDatabase(databaseName: "InMemoryDatabase")
+			.Options;
+			return new ApplicationDbContext(options);
 		}
 
 	}
