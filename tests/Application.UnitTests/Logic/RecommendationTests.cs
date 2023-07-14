073d1987dc89c961f9265d24e1ba3ffffc67a305
Stephanie Kramer <s.kramer2@hva.nl>
2023-07-14 15:44:16 +0200

    Finish unit tests for activity recommendations

--- tests/Application.UnitTests/Logic/RecommendationTests.cs
+++ tests/Application.UnitTests/Logic/RecommendationTests.cs
@@ -1,27 +1,12 @@
-using System;
-using Microsoft.EntityFrameworkCore;
-using Moq;
 using NUnit.Framework;
-using Sonuts.Application.Common.Interfaces;
 using Sonuts.Application.Logic.Categories.Queries;
-using Sonuts.Domain.Common;
 using Sonuts.Domain.Entities;
 using Sonuts.Domain.Enums;
-using Sonuts.Infrastructure.Persistence;
 
 namespace Sonuts.Application.UnitTests.Logic
 {
-	public class RecommendationTests
+	public class ActivityRecommendationTests
 	{
-		public RecommendationTests()
-		{
-		}
-
-		[SetUp]
-		public void Init()
-		{
-		}
-
 		[Test]
 		public void IsRecommendedThemeCalcuationTest_Balance_Exercises()
 		{
@@ -53,23 +38,21 @@ namespace Sonuts.Application.UnitTests.Logic
 			RecommendationRule[] ruleArray = new RecommendationRule[1];
 			ruleArray[0] = balanceExercises;
 
-			var questionResponse = CreateQuestionResponseForBalancingExercises();
+			var questionResponse = CreateQuestionResponseForBalancingExercises(question);
 			QuestionResponse[] questionResponses = new QuestionResponse[1];
 			questionResponses[0] = questionResponse;
 
-			var expected = false;
 			//Act
-
 			GetCategoriesQueryHandler getCategoriesQueryHandler = new GetCategoriesQueryHandler();
 			var actual = getCategoriesQueryHandler.IsRecommendedThemeCalculation(questionResponses, ruleArray);
 
 			//Assert
+			var expected = false;
 			Assert.AreEqual(expected, actual);
 		}
 
-		private QuestionResponse CreateQuestionResponseForBalancingExercises()
+		private QuestionResponse CreateQuestionResponseForBalancingExercises(Question question)
 		{
-
 			var participant = new Participant
 			{
 				FirstName = "Jane",
@@ -80,21 +63,6 @@ namespace Sonuts.Application.UnitTests.Logic
 				Height = 178,
 				MaritalStatus = "Married",
 				IsActive = true
-			};
-
-			var question = new Question
-			{
-				Type = QuestionType.Integer,
-				Text = "Number of days per week balancing exercises",
-				Description = null,
-				Information = null,
-				Order = 29,
-				EnableWhen = null,
-				AnswerOptions = null,
-				OpenAnswerLabel = null,
-				IsRequired = true,
-				Min = 0,
-				Max = 7
 			};
 
 			var questionnaire = new Questionnaire
@@ -118,7 +86,6 @@ namespace Sonuts.Application.UnitTests.Logic
 			};
 			questionnaireResponse.Responses.Add(response);
 			response.QuestionnaireResponse = questionnaireResponse;
-
 			return response;
 		}
 
@@ -141,33 +108,32 @@ namespace Sonuts.Application.UnitTests.Logic
 				Max = 7
 			};
 
-			RecommendationRule balanceExercises = new RecommendationRule
+			RecommendationRule boneExercises = new RecommendationRule
 			{
 				Type = RecommendationRuleType.All,
 				Operator = Operator.GreaterOrEquals,
 				Value = "2",
 			};
 
-			balanceExercises.Questions.Add(question);
+			boneExercises.Questions.Add(question);
 
 			RecommendationRule[] ruleArray = new RecommendationRule[1];
-			ruleArray[0] = balanceExercises;
+			ruleArray[0] = boneExercises;
 
-			var questionResponse = CreateQuestionResponseForBoneExercises();
+			var questionResponse = CreateQuestionResponseForBoneExercises(question);
 			QuestionResponse[] questionResponses = new QuestionResponse[1];
 			questionResponses[0] = questionResponse;
 
-			var expected = true;
 			//Act
-
 			GetCategoriesQueryHandler getCategoriesQueryHandler = new GetCategoriesQueryHandler();
 			var actual = getCategoriesQueryHandler.IsRecommendedThemeCalculation(questionResponses, ruleArray);
 
 			//Assert
+			var expected = true;
 			Assert.AreEqual(expected, actual);
 		}
 
-		private QuestionResponse CreateQuestionResponseForBoneExercises()
+		private QuestionResponse CreateQuestionResponseForBoneExercises(Question question)
 		{
 
 			var participant = new Participant
@@ -180,21 +146,6 @@ namespace Sonuts.Application.UnitTests.Logic
 				Height = 178,
 				MaritalStatus = "Married",
 				IsActive = true
-			};
-
-			var question = new Question
-			{
-				Type = QuestionType.Integer,
-				Text = "Number of days per week bone-strengthening exercises",
-				Description = null,
-				Information = null,
-				Order = 35,
-				EnableWhen = null,
-				AnswerOptions = null,
-				OpenAnswerLabel = null,
-				IsRequired = true,
-				Min = 0,
-				Max = 7
 			};
 
 			var questionnaire = new Questionnaire
@@ -218,18 +169,91 @@ namespace Sonuts.Application.UnitTests.Logic
 			};
 			questionnaireResponse.Responses.Add(response);
 			response.QuestionnaireResponse = questionnaireResponse;
-
 			return response;
 		}
 
-		public ApplicationDbContext GetMemoryContext()
+		[Test]
+		public void IsRecommendedThemeCalcuationTest_Muscle_Exercises()
 		{
-			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-			.UseInMemoryDatabase(databaseName: "InMemoryDatabase")
-			.Options;
-			return new ApplicationDbContext(options);
+			//Arrange
+			var question = new Question
+			{
+				Type = QuestionType.Integer,
+				Text = "Number of days per week muscle-strengthening exercises",
+				Description = null,
+				Information = null,
+				Order = 32,
+				EnableWhen = null,
+				AnswerOptions = null,
+				OpenAnswerLabel = null,
+				IsRequired = true,
+				Min = 0,
+				Max = 7
+			};
+
+			RecommendationRule muscleExercises = new RecommendationRule
+			{
+				Type = RecommendationRuleType.All,
+				Operator = Operator.GreaterOrEquals,
+				Value = "2",
+			};
+
+			muscleExercises.Questions.Add(question);
+
+			RecommendationRule[] ruleArray = new RecommendationRule[1];
+			ruleArray[0] = muscleExercises;
+
+			var questionResponse = CreateQuestionResponseForMuscleExercises(question);
+			QuestionResponse[] questionResponses = new QuestionResponse[1];
+			questionResponses[0] = questionResponse;
+
+			//Act
+			GetCategoriesQueryHandler getCategoriesQueryHandler = new GetCategoriesQueryHandler();
+			var actual = getCategoriesQueryHandler.IsRecommendedThemeCalculation(questionResponses, ruleArray);
+
+			//Assert
+			var expected = false;
+			Assert.AreEqual(expected, actual);
 		}
 
+		private QuestionResponse CreateQuestionResponseForMuscleExercises(Question question)
+		{
+
+			var participant = new Participant
+			{
+				FirstName = "Jane",
+				LastName = "Doe",
+				Birth = new DateOnly(year: 1958, month: 10, day: 20),
+				Gender = "Female",
+				Weight = 76,
+				Height = 178,
+				MaritalStatus = "Married",
+				IsActive = true
+			};
+
+			var questionnaire = new Questionnaire
+			{
+				Title = "Questions about exercise",
+				Description = "Questions about exercise"
+			};
+			questionnaire.Questions.Add(question);
+
+			var response = new QuestionResponse
+			{
+				Question = question,
+				Answer = "6"
+			};
+
+			var questionnaireResponse = new QuestionnaireResponse
+			{
+				CreatedAt = new DateOnly(year: 2023, month: 07, day: 14),
+				Questionnaire = questionnaire,
+				Participant = participant,
+			};
+			questionnaireResponse.Responses.Add(response);
+			response.QuestionnaireResponse = questionnaireResponse;
+			return response;
+		}
 	}
-	}
+}
 
